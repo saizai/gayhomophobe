33b52476b942a7ca0adfdf3f24cdce9522a3d147
Sai <github@saizai.com>
2021-01-13 11:06:34 +0000

    Delete sync.sh

--- sync.sh
+++
@@ -1,4 +0,0 @@
-git -C ~/GoogleDrive/workspace/gayhomophobe.com/ add ~/GoogleDrive/workspace/gayhomophobe.com/
-git -C ~/GoogleDrive/workspace/gayhomophobe.com/ commit -am "update"
-git -C ~/GoogleDrive/workspace/gayhomophobe.com/ push
-rsync -vrSzhPc --exclude="- .git" ~/GoogleDrive/workspace/gayhomophobe.com/ gayhomophobe@gayhomophobe.com:~/gayhomophobe.com
